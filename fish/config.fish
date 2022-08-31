bb6a1a015869ff2044be9ecf067edbde6cf348ac
Rohan Jeendgar <rohanjeendgar@gmail.com>
2022-08-31 15:18:53 +0530

    Add config files

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+neofetch
+fish_add_path /home/rohanj/bin
+fish_add_path ~/.emacs.d/bin
+fish_add_path /home/rohanj/.config/rofi/scripts/
